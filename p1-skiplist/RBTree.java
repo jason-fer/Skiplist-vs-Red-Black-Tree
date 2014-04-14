45c911d8acfb90bdf62cb79b7567b43547f6b821
Jason Feriante <jason.feriante@gmail.com>
2014-04-14 10:58:38 -0500

    RBTree update

--- p1-skiplist/RBTree.java
+++ p1-skiplist/RBTree.java
@@ -51,6 +51,17 @@ public class RBTree<K extends Comparable <K>> {
 	// complete this method
 	private RBnode<K> insert(RBnode<K> n, K key) throws DuplicateException {
 		return n;
+		// 1- insert leaf with BST insert algorithm (to add K to tree)
+		// 2- color the node containing K red
+		// 3- restore red black tree properties (if needed)
+		/**
+		 * BST Insert:
+		 * -step down tree as in lookup
+		 * -if N's key equals key, throw duplicate exception
+		 * -when we get to the end of the tree where lookup would expect to
+		 * find a node w/ key, we insert into new leaf onto the appropriate
+		 * side.
+		 */
 		/**
 		 * Goal: insert key value K into tree T and maintain RBT properties.
 		 * Case: T is empty -- add a new black leaf node containing K
@@ -67,6 +78,37 @@ public class RBTree<K extends Comparable <K>> {
 
 	// complete this method
 	private RBnode<K> delete(RBnode<K> n, K key){
+		/**
+		 * BST Delete:
+		 * -Step down tree as in lookup
+		 * -If N is null, return NULL/ not found
+		 * -If N's key equals key...
+		 * 		case 1 (easy): n is a leaf w/ 0 children
+		 * 			-delete N by setting the appropriate child of N's parent P
+		 * 			to null
+		 * 		case 2 (easy): n has one child.
+		 * 			-link parent to n's child
+		 * 			-set appropriate child of n's parent to his child c
+		 * 		case 3 (difficult): n has 2 children
+		 * 			-parent cannot hold on to both of A's children; it has only 
+		 * 			the one reference 
+		 * 			soln: find a replacement value v in either n's left or right
+		 * 			subtree.
+		 * 				-copy v to n's key
+		 * 				-recursively deleting v in n's subtree
+		 * 
+		 * 		w/t can we use as n's replacement:
+		 * 		soln: 2 replacements work
+		 * 			in-order predecessor
+		 * 				"largest value in left subtree"
+		 * 				-step into left subtree, go as far right as possible		
+		 * 			in-order successor
+		 * 				"smallest value in right subrtree"
+		 * 				-step into right subtree, then go as far left as possible
+		 * 		*Both of these are guaranteed to have at most one child, 
+		 * 		making delete an easy case.
+		 * 						
+		 */
 		return n;
 	}
 	
