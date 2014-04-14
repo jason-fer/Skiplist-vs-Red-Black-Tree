5c6d98d1c298bd24a3284ce9a3db913541987ddd
Jason Feriante <jason.feriante@gmail.com>
2014-04-14 00:00:50 -0500

    Working on RBTree

--- p1-skiplist/RBTree.java
+++ p1-skiplist/RBTree.java
@@ -1,13 +1,16 @@
 import java.io.PrintStream;
 
 /**
- * The RBTree is a height balanced tree. With binary search trees the average
- * case is O(Log N) for insert, lookup & delete, where N is the number of nodes 
- * in the tree; worst case times are O(N). The height balanced RBTree allows us 
- * to guarantee O(N) time for all three methods. Adapted from the BST Class as 
- * taught in CS 367. 
+ * The RBTree is a height balanced tree, invented in the 1970s it was called a 
+ * "symmetric binary btree". With binary search trees the average case is 
+ * O(Log N) for insert, lookup & delete, where N is the number of nodes in the 
+ * tree; worst case times are O(N). The height balanced RBTree allows us to 
+ * guarantee O(N) time for all three methods. Adapted from the BST Class as 
+ * taught in CS 367. Note: Items used must be from a class with the comparable 
+ * interface. 
  * 
- * Note: Items used must be from a class with the comparable interface.
+ * Derive complexity: n = 2^n - 1, n + 1 = 2^h, log(n+1) = h, O(log(n+1)),
+ * ... asymptotically: O(log N)
  * @author Jason Feriante
  */
 public class RBTree<K extends Comparable <K>> {
@@ -16,7 +19,7 @@ public class RBTree<K extends Comparable <K>> {
 	
 	public RBTree() { root = null; }
 	
-	// Red property: Children of a red node are black
+	// Red property: red nodes must have black children
 	
 	// Black property: every path from root to leaf must have the same number
 	// of black nodes
@@ -49,7 +52,16 @@ public class RBTree<K extends Comparable <K>> {
 	private RBnode<K> insert(RBnode<K> n, K key) throws DuplicateException {
 		return n;
 		/**
-		 * BST algorithm:
+		 * Goal: insert key value K into tree T and maintain RBT properties.
+		 * Case: T is empty -- add a new black leaf node containing K
+		 * Case: T is non-empty
+		 * 	-search for location to insert as done for BST
+		 *  -add K as a red leaf node
+		 *  -restore red-black tree properties if red property violation
+		 *  	Case 1: K's parent P is black - done
+		 *  	Case 2: K's parent P is red (red property violation)
+		 *  		a: P's sibling S is black or null - trinode restructure. done.
+		 *  		b: P's sibling is red -- recolor, may need to recursively restore. 
 		 */
 	}
 
