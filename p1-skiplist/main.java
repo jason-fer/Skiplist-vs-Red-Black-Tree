92d0bbb9be8c769ff3c5c127d6fd96f427dcba66
Jason Feriante <jason.feriante@gmail.com>
2014-04-21 01:47:01 -0500

    Red Black Tree: we are now using Sedgewick's version. It's done so we can start our analysis right away.....

--- p1-skiplist/main.java
+++ p1-skiplist/main.java
@@ -6,67 +6,25 @@ public class main {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		Integer a = 10;
-//		Integer b = 100;
-//		int rs = a.compareTo(b);
-//		System.out.println("a:"+a+" vs b:"+b+", result:"+rs);
-//		
-//		a = 20;
-//		b = 10;
-//		rs = a.compareTo(b);
-//		System.out.println("a:"+a+" vs b:"+b+", result:"+rs);
-//		
-//		a = 10;
-//		b = 10;
-//		rs = a.compareTo(b);
-//		System.out.println("a:"+a+" vs b:"+b+", result:"+rs);
-		
+
 		testRBTree();
 	}
 	
 	public static void testRBTree() {
-		// @todo (confirm the red-black tree works correctly....)
+    	RedBlackBST<Integer, Integer> st = new RedBlackBST<Integer, Integer>();
+    	
+        for (int i = 0;i < 50; i++) { st.put(i, i); }
+
+        for (Integer s : st.keys())
+            System.out.println(st.get(s));
+        System.out.println();
+        
+//		System.out.println(rbtree.toString());
 		
-		RedBlackTree_orig<Integer> rbtree = new RedBlackTree_orig<Integer>();
-		try {
-			int i = 1;
-			while(i <= 6){
-				rbtree.insert(i);
-//				if(i == 5) { rbtree.print(); System.out.println("-----------------------------------------------------"); }
-//				rbtree.print();
-//				System.out.println("-----------------------------------------------------");
-//				System.out.println("insert i:"+i);
-				i = i + 1;
-			}
-//			rbtree.insert(50);
-//			rbtree.insert(20);
-//			rbtree.insert(30);
-//			rbtree.insert(40);
-//			rbtree.insert(25);
-//			rbtree.insert(35);
-//			rbtree.insert(90);
-//			rbtree.insert(100);
-//			rbtree.insert(110);
-//			rbtree.insert(11);
-//			rbtree.insert(13);
-			
-//			rbtree.insert(12);
-//			rbtree.insert(23);
-//			rbtree.insert(24);
-		} catch (DuplicateException e) {
-			e.printStackTrace();
-		}
-		// Should show: 10,20,25,30,35
-		System.out.println("Legend: [XX] indicates a red node.");
-		System.out.println("");
-		rbtree.print();
-		System.out.println("");
-		System.out.println("count: "+rbtree.count());
-		System.out.println("height: "+rbtree.height());
-		System.out.println("min: "+rbtree.min());
-		System.out.println("max: "+rbtree.max());
+//		System.out.println("count: "+rbtree.size());
+//		System.out.println("height: "+rbtree.height());
+//		System.out.println("min: "+rbtree.min());
+//		System.out.println("max: "+rbtree.max());
 	}
 
 }
-//
