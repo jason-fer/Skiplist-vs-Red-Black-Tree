faf216f3f0faa8f707a79cffa731dc2a8b1e7035
Jing Li <lijing@mumble-30.cs.wisc.edu>
2014-04-27 14:25:31 -0500

    Skiplist concurrency work

--- p1-skiplist/TestProg.java
+++ p1-skiplist/TestProg.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 
 public class TestProg
@@ -6,23 +8,29 @@ public class TestProg
 
    public static void main(String[] args)
    {
-
-      Random r = new Random();
-      SkipList S = new SkipList();
-
-      int i;
-
-
-      for ( i = 0; i < 12; i++ )
-      {
-         S.insert("" + i,  i);
-      }
-      S.remove("4", 4);
-      S.remove("7", 7);
-//    S.printHorizontal();
-//    System.out.println("------");
-      S.printVertical();
-
+	  final SkipList S = new SkipList();
+	  final Lock lock = new ReentrantLock();
+	  for(int j=0;j<2;j++){
+		  new Thread(new Runnable(){ 
+			  public void run() { 
+				  int i;
+				  lock.lock();//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+				  for ( i = 0; i < 100; i++ )
+			      {
+			         S.insert("" + i,  i);
+			      }
+				  lock.unlock();//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+				  System.out.println(Thread.currentThread().getName()+": "+S.lockNum+"    ");
+			  }
+		  }).start();
+	  }
+	  
+//      S.remove("4", 4);
+//      S.remove("7", 7);
+//      System.out.println("------");
+//      S.insert("" + 15,  15);
+//      S.printVertical();
+//      System.out.println("haha  "+S.lockNum+"    "+S.lockNumH);
    }
 
 }
